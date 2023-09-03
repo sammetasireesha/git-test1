c75c08dbb6d815ab27f47a35497e8641791a7ee7
unknown <sireeshasammeta>
2023-09-03 02:43:45 -0400

    modified server.js

--- my-server.js
+++ my-server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.write('Hello World from Sammeta Sireesha!');
+  res.write('Hello World from Sammeta !');
   res.end();
 }).listen(3000, function(){
  console.log("Server running at port 3000");
